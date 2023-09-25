dfc7e5ccaa5aeaaaac5a9b4eab0c5f44851c51b2
yatin yadav <49709956+yatinrao16@users.noreply.github.com>
2023-09-25 11:16:07 +0530

    Update Signup.js

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -31,7 +31,7 @@ function SignUp(props) {
     const handleClick = async (e) => {
         const { email, name, password, cpassword } = credentials;
 
-        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+        const response = await fetch("https://inotebookonline.onrender.com/api/auth/createuser", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
