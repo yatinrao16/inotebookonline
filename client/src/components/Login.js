c2d22a79bc6b67060c78b820cf8d604a0e2773cc
yatin yadav <49709956+yatinrao16@users.noreply.github.com>
2023-09-25 11:16:39 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -23,7 +23,7 @@ function Login(props) {
   };
 
   const handleClick = async () => {
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://inotebookonline.onrender.com/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
